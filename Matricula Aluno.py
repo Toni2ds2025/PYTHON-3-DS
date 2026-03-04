df348bc5562526d8a702917c2c66a22023a237ca
Aka Akasaka <antonio.kuhl.neto@escola.pr.gov.br>
2026-03-04 09:24:42 -0300

    Add files via upload

--- Matricula Aluno.py
+++ Matricula Aluno.py
@@ -11,13 +11,29 @@ class Aluno:
     def calcular_media(self):
         if not self.notas:
             return 0
-        return sum(self.notas.value()) / len(self.notas)
+        return sum(self.notas.values()) / len(self.notas)
 
     def exibir_informacoes(self):
         print(f"Nome: {self.nome}")
         print(f"Matricula: {self.matricula}")
-        print(f"Curso {self.curso}")
+        print(f"Curso: {self.curso}")
         print("Notas: ")
         for disciplina, nota in self.notas.items():
-            print(f" {disciplina}: {notas}")
+            print(f" {disciplina}: {nota}")
         print(f"Média: {self.calcular_media():.2f}")
+
+# Criando objetos alunos
+aluno1 = Aluno("João", "202312345", "Engenharia")
+aluno2 = Aluno("Maria", "202367890", "Ciências da Computação")
+
+#Adicionando notas
+aluno1.adicionar_nota("Matemática", 8.5)
+aluno1.adicionar_nota("Física", 7.0)
+aluno2.adicionar_nota("Programação", 9.0)
+aluno2.adicionar_nota("Banco de Dados", 8.0)
+
+#Exibindo informações
+aluno1.exibir_informacoes()
+print("-" * 20)
+aluno2.exibir_informacoes()
+print("-" * 20)
