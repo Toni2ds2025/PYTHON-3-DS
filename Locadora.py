4d374901feddad7c186bad307f49043f7f7fbed6
Aka Akasaka <antonio.kuhl.neto@escola.pr.gov.br>
2026-03-12 10:36:44 -0300

    Add files via upload

--- Locadora.py
+++ Locadora.py
@@ -37,7 +37,47 @@ class Locadora:
         cliente = next((v for v in self.veiculos if v.placa == placa), None)
 
         if cliente and veiculo and veiculo.disponivel:
-            veiculo.disponive = False
+            veiculo.disponivel = False
             self.locacoes[placa] = cpf
             return f"Veiculo {Veiculo.modelo} alugado para {cliente.nome}."
         return "Erro ao alugar veículo. Verifique os dados."
+    
+    def devolver_veiculo(self, placa):
+        veiculo = next((v for v in self.veiculos if v.placa == placa), None)
+    
+        if veiculo and not veiculo.disponivel:
+            veiculo.disponivel = True
+            self.locacoes.pop(placa, None)
+            return f"Veiculo {veiculo.modelo} devolvido com sucesso."
+        return "Erro ao devolver veiculo. Verifique os dados."
+    
+    def listar_veiculos(self, disponiveis=True):
+        return [str(v) for v in self.veiculos if v.disponivel == disponiveis]
+    
+class App:
+    def __init__(self, root):
+        self.locadora = Locadora()
+        self.root = root
+        self.root.title("Locadora de veiculos")
+
+        tk Button(root, text="Cadastrar Veiculos", command=self.cadastrar_veiculo).pack(fill='x')
+        tk Button(root, text="Cadastrar Cliente", command=self.cadastrar_cliente).pack(fill='x')
+        tk Button(root, text="Alugar Veiculo", command=self.alugar_veiculo).pack(fill='x')
+        tk Button(root, text="Devolver Veiculos", command=self.devolver_veiculo).pack(fill='x')
+        tk Button(root, text="Listar Veiculos Disponíveis", command=self.listar_alugados).pack(fill='x')
+        tk Button(root, text="Listar Veiculos Alugados", command=self.listar_alugados).pack(fill='x')
+
+    def cadastrar_veiculo(self):
+        placa = simpledatalog.askstring("Cadastro", "Placa:")
+        modelo = simpledatalog.askstring("Cadastro", "Modelo:")
+        ano = simpledatalog.askstring("Cadastro", "Ano:")
+        if placa and modelo and ano:
+            self.locadora.cadastrar_veiculo(Veiculo(placa, modelo, ano))
+            messagebox.showinfo("Sucesso", "Veiculo cadastrado!")
+
+    def cadastrar_cliente(self):
+        nome = simpledatalog.askstring("Cadastro", "Nome:")
+        cpf = simpledatalog.askstring("Cadastro", "CPF:")
+        if nome and cpf:
+            self.locadora.cadastrar_cliente(Cliente(nome, cpf))
+            messagebox.showinfo("Sucesso", "Cliente cadastrado!")
